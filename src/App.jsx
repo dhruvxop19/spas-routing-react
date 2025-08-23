3d4192d606c9abd438ee2922554eb7853e2e3037
dhruvxop19 <dhruvxop19@gmail.com>
2025-08-23 22:35:30 +0530

    added Link instead of a tag for spa's

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 
 function App() {
   return <div>
-    <a href="/">Allen</a>
-    |
-    <a href="/neet/online-coaching-class-11">Class 11</a>
-    |
-    <a href="/neet/online-coaching-class-12">Class 12</a>
+
     <BrowserRouter>
+      <Link to="/">Allen</Link>
+      |
+      <Link to="/neet/online-coaching-class-11">Class 11</Link>
+      |
+      <Link to="/neet/online-coaching-class-12">Class 12</Link>
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/neet/online-coaching-class-11" element={<Class11Program />} />
