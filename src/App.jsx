1bf64e21264687267c1428486935358ab3bf3311
dhruvxop19 <dhruvxop19@gmail.com>
2025-08-23 22:26:40 +0530

    add three things which leads you directly to routes by just clicking on them

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
   return <div>
+    <a href="/">Allen</a>
+    |
+    <a href="/neet/online-coaching-class-11">Class 11</a>
+    |
+    <a href="/neet/online-coaching-class-12">Class 12</a>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/neet/online-coaching-class-11" element={<Class11Program />} />
-        <Route path="/neet/online-coaching-class12" element={<Class12Program />} />
+        <Route path="/neet/online-coaching-class-12" element={<Class12Program />} />
       </Routes>
     </BrowserRouter>
-  </div>
+  </div >
 }
 
 function Landing() {
@@ -23,10 +28,12 @@ function Class11Program() {
     Class 11 content
   </div>
 
-  function Class12Program() {
-    return <div>
-      Class 12 content
-    </div>
-  }
 }
+
+function Class12Program() {
+  return <div>
+    Class 12 content
+  </div>
+}
+
 export default App
